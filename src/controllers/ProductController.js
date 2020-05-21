172f35bbfe8702ad6621f87b5e3ce4d0253e6b87
samuelffn <samuelffn@gmail.com>
2020-05-21 16:34:02 -0300

    Add mais métodos para crud

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -2,15 +2,34 @@ const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
 module.exports = {
+    
+    // Listar todos
     async index(req, res) {
         const products = await Product.find();
-
         return res.json(products);
     },
 
+    // Detalhe
+    async show(req, res) {
+        const product = await Product.findById(req.params.id);
+        return res.json(product);
+    },
+
+    // Salvar
     async store(req, res) {
         const product = await Product.create(req.body); // req.body contém o json que enviamos na requisição
-
         return res.json(product); // Retorna o produto criado
+    },
+
+    // Atualizar
+    async update(req, res) {
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true }); // o new é para que ele retorne a informação atualizada
+        return res.json(product);
+    },
+
+    // Excluir
+    async destroy(req, res) {
+        await Product.findByIdAndRemove(req.params.id);
+        return res.send();
     }
 };
